261219babba79578923d9c4cb77465c5b333c87b
Joseph-Emmanuel Banzio <joseph.banzio@contentsquare.com>
2023-12-08 17:32:22 +0100

    feat: plug postgres DB with docker ch5.3

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -15,6 +20,9 @@ const version = "1.0.0"
 type Config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 // Holds dependencies for HTTP handlers, helpers and middlewares
@@ -24,13 +32,27 @@ type Application struct {
 }
 
 func main() {
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 	var cfg Config
+
+	err := godotenv.Load("./.env")
+	if err != nil {
+		logger.Fatal("Error loading .env file")
+	}
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("DATABASE_URL"), "PostgreSQL DSN")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	db, err := openDB(cfg)
+
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
+
+	logger.Printf("database connection pool established")
 
 	app := &Application{
 		config: cfg,
@@ -46,7 +68,26 @@ func main() {
 	}
 
 	logger.Printf("Starting %s server running on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 
+}
+
+func openDB(cfg Config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// context with 5 second timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/jbnzi0/greenlight
 
 go 1.20
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+)
